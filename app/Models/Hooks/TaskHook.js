68c0afde9d7da691849812dd12815e49cf1b2060
Keepler <arturnunespe@gmail.com>
2020-04-16 10:46:30 -0300

    Mailing queue

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,33 +1,19 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
-
-const TaskHook = (exports = module.exports = {})
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 
 async function sendMail(modelInstance) {
+  console.log(1)
   const { title } = modelInstance
 
   const { username, email } = await modelInstance.user().fetch()
   const file = await modelInstance.file().fetch()
 
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from('noreply@adonisapi.dev', 'Adonis REST API')
-        .subject("There's a task for you")
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { email, username, file, title })
 }
+
+const TaskHook = (exports = module.exports = {})
 
 TaskHook.sendMailAfterCreate = async modelInstance => {
   if (modelInstance.user_id) await sendMail(modelInstance)
