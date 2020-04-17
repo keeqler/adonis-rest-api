165c9af15d761185bc3fbb6dc403e9f2a20977fa
Keepler <arturnunespe@gmail.com>
2020-04-17 03:59:12 -0300

    Fix controller error handling

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -2,7 +2,9 @@
 
 const Raven = require('raven')
 
+const Env = use('Env')
 const Config = use('Config')
+const Youch = use('youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
 
 /**
@@ -12,9 +14,17 @@ const BaseExceptionHandler = use('BaseExceptionHandler')
  * @class ExceptionHandler
  */
 class ExceptionHandler extends BaseExceptionHandler {
-  async handle(error, { response }) {
+  async handle(error, { request, response }) {
     if (error.name === 'ValidationException')
-      response.status(error.status).send(error.messages)
+      return response.status(400).send(error.messages)
+
+    if (Env.get('NODE_ENV') === 'development') {
+      const youch = new Youch(error, request.request)
+
+      return response.status(500).send(await youch.toJSON())
+    }
+
+    return response.status(500)
   }
 
   async report(error) {
